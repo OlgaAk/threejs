0bfa60c3d8e5dbab2067f96dc085a4abaacbbf32
okokhan <olga.kokhanova@t-systems.com>
2021-08-16 11:59:34 +0300

    files moved from node modules

--- DragControls.js
+++ DragControls.js
@@ -5,7 +5,7 @@ import {
 	Raycaster,
 	Vector2,
 	Vector3
-} from '../node_modules/three/build/three.module.js';
+} from './three.module.js';
 
 const _plane = new Plane();
 const _raycaster = new Raycaster();
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import * as THREE from '../node_modules/three/build/three.module.js';
+import * as THREE from './three.module.js';
 import {DragControls} from './DragControls.js';
 
 let renderer, scene, camera, raycaster, controls, mouse;
