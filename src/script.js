9ae11d149cf96d117b9bb019da338f7dd1e55253
okokhan <olga.kokhanova@t-systems.com>
2021-09-04 15:01:38 +0300

    threejs version upgrade otherwise geometryutils error

--- src/script.js
+++ src/script.js
@@ -1,86 +1,95 @@
-import * as THREE from '../node_modules/three/build/three.module.js';
-import {projectScene} from "./ProjectScene"
-import {PolygonFactory} from "./PolygonFactory"
+// import * as THREE from '../node_modules/three/build/three.module.js';
+// import {projectScene} from "./ProjectScene"
+// import {PolygonFactory} from "./PolygonFactory"
+//
+//
+// let selectedShapeTab, selectedShape;
+// let shapes = []
+// let shapeTabs
+//
+// projectScene.initScene()
+//
+// initMenu()
+//
+// const polygonFactory = new PolygonFactory()
+//
+// initEventListeners()
+//
+// // polygon.init()
+//
+//
+// function initMenu() {
+//     shapeTabs = document.querySelectorAll(".shape")
+//     shapeTabs.forEach(e => {
+//         e.addEventListener("click", selectTab)
+//     })
+// }
+//
+// function selectTab(event) {
+//     changeTabColor(event)
+//     selectShape(event)
+// }
+//
+// function changeTabColor() {
+//     if (selectedShapeTab != undefined) selectedShapeTab.style.backgroundColor = "lightgray"
+//     selectedShapeTab = event.target
+//     event.target.style.backgroundColor = "darkolivegreen"
+// }
+//
+// function selectShape(event) {
+//     shapes.forEach(o=> projectScene.removeObjectFromScene(o))
+//     shapes = []
+//     switch (selectedShapeTab.id) {
+//         case "singleGeometryPolygon":
+//             selectedShape = polygonFactory.create("singleGeometry")
+//             projectScene.addObjectToScene(selectedShape)
+//             shapes.push(selectedShape)
+//             break
+//         case "multiGeometryPolygon":
+//             selectedShape = polygonFactory.create("multiGeometry")
+//             projectScene.addObjectToScene(selectedShape)
+//             shapes.push(selectedShape)
+//             break
+//     }
+// }
+//
+//
+// function initEventListeners() {
+//     window.addEventListener("mousedown", mouseDown, false);
+//     window.addEventListener("mousemove", mouseMove, false);
+//     window.addEventListener("mouseup", mouseUp, false);
+//     document.querySelector("canvas").addEventListener('click', canvasClickClickHandler, false);
+// }
+//
+//
+// function canvasClickClickHandler(event){
+//     if(selectedShape==undefined) {
+//         alert("Please select a shape")
+//         return
+//     }
+//     selectedShape.onClickHandler(event)
+// }
+//
+// function mouseDown(event){
+//     if(selectedShape==undefined) return
+//     selectedShape.mouseDown(event)
+// }
+//
+// function mouseMove(event){
+//     if(selectedShape==undefined) return
+//     selectedShape.mouseMove(event)
+// }
+//
+// function mouseUp(event){
+//     if(selectedShape==undefined) return
+//     selectedShape.mouseUp(event)
+// }
 
 
-let selectedShapeTab, selectedShape;
-let shapes = []
-let shapeTabs
-
-projectScene.initScene()
-
-initMenu()
-
-const polygonFactory = new PolygonFactory()
-
-initEventListeners()
-
-// polygon.init()
 
 
-function initMenu() {
-    shapeTabs = document.querySelectorAll(".shape")
-    shapeTabs.forEach(e => {
-        e.addEventListener("click", selectTab)
-    })
-}
 
-function selectTab(event) {
-    changeTabColor(event)
-    selectShape(event)
-}
+import {init, animate} from "./FatLine"
 
-function changeTabColor() {
-    if (selectedShapeTab != undefined) selectedShapeTab.style.backgroundColor = "lightgray"
-    selectedShapeTab = event.target
-    event.target.style.backgroundColor = "darkolivegreen"
-}
-
-function selectShape(event) {
-    shapes.forEach(o=> projectScene.removeObjectFromScene(o))
-    shapes = []
-    switch (selectedShapeTab.id) {
-        case "singleGeometryPolygon":
-            selectedShape = polygonFactory.create("singleGeometry")
-            projectScene.addObjectToScene(selectedShape)
-            shapes.push(selectedShape)
-            break
-        case "multiGeometryPolygon":
-            selectedShape = polygonFactory.create("multiGeometry")
-            projectScene.addObjectToScene(selectedShape)
-            shapes.push(selectedShape)
-            break
-    }
-}
-
-
-function initEventListeners() {
-    window.addEventListener("mousedown", mouseDown, false);
-    window.addEventListener("mousemove", mouseMove, false);
-    window.addEventListener("mouseup", mouseUp, false);
-    document.querySelector("canvas").addEventListener('click', canvasClickClickHandler, false);
-}
-
-
-function canvasClickClickHandler(event){
-    if(selectedShape==undefined) {
-        alert("Please select a shape")
-        return
-    }
-    selectedShape.onClickHandler(event)
-}
-
-function mouseDown(event){
-    if(selectedShape==undefined) return
-    selectedShape.mouseDown(event)
-}
-
-function mouseMove(event){
-    if(selectedShape==undefined) return
-    selectedShape.mouseMove(event)
-}
-
-function mouseUp(event){
-    if(selectedShape==undefined) return
-    selectedShape.mouseUp(event)
-}
+init()
+animate()
